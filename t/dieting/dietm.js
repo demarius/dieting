3476e69c0a1132fb14308dc3807f2fb415e3ae33
Demarius Chrite <chrite.demarius@gmail.com>
2016-01-19 23:18:46 -0400

    Create initial mocha test.

---
+++ t/dieting/dietm.js
@@ -0,0 +1,8 @@
+var assert = require('assert')
+
+describe('Dieting', function () {
+    describe('#init()', function () {
+        it('should start a server and listen on a socket', function () {
+        })
+    })
+})
