c3f47ef834d09358089ac256c61f2f24038a2544
Demarius Chrite <chrite.demarius@gmail.com>
2016-04-21 11:33:41 -0400

    Remove unused test file.

--- t/dieting/dietm.js
+++
@@ -1,8 +0,0 @@
-var assert = require('chai').assert
-
-describe('Dieting', function () {
-    describe('#init()', function () {
-        it('should start a server and listen on a socket', function () {
-        })
-    })
-})
